18b4f1144d67b011b7be0e7138f92aad2ade3e7b
jyLu <lujiyang@trophytech.net>
2016-07-12 11:51:33 +0800

    添加图标并设置只扫描卡号
    添加图标并设置只扫描卡号

--- cardScan/ViewController.m
+++ cardScan/ViewController.m
@@ -25,11 +25,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+- (IBAction)scanCard:(id)sender {
     CardIOPaymentViewController *scanViewController = [[CardIOPaymentViewController alloc] initWithPaymentDelegate:self];
+    scanViewController.collectExpiry = NO;
+    scanViewController.collectCVV = NO;
     [self presentViewController:scanViewController animated:YES completion:nil];
 }
+
 
 - (void)userDidCancelPaymentViewController:(CardIOPaymentViewController *)scanViewController {
     NSLog(@"User canceled payment info");
